8f88c9853817a8586738137691dbe02b9d184e10
mh4746 <60458977+mh4746@users.noreply.github.com>
2022-08-08 21:48:23 +0600

    Add files via upload

---
+++ AbstractClass.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Root
+ */
+
+abstract class Abc{
+    
+   abstract void run();
+
+}
+
+class AbstractClass extends Abc{
+ 
+public static void main(String args[])
+{
+      System.out.println("Checking Abstract ");
+   
+} 
+   
+}
